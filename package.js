fd4b202abeb1b137ce028955b7a046371c186695
Pete Corey <petecorey@gmail.com>
2015-03-08 13:35:34 -0700

    Updated package info and deps

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
-  name: 'pcorey:orion-slingshot',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'pcorey:orion-slingshot',
+    version: '0.0.1',
+    summary: 'Orion file provider wrapper around Slingshot',
+    git: 'https://github.com/pcorey/orion-slingshot',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.2');
-  api.addFiles('pcorey:orion-slingshot.js');
+    api.versionsFrom('1.0.3.2');
+    api.use([
+        'orionjs:core',
+        'orionjs:filesystem'
+    ]);
+    api.addFiles('pcorey:orion-slingshot.js');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('pcorey:orion-slingshot');
-  api.addFiles('pcorey:orion-slingshot-tests.js');
+    api.use('tinytest');
+    api.use('pcorey:orion-slingshot');
+    api.addFiles('pcorey:orion-slingshot-tests.js');
 });
